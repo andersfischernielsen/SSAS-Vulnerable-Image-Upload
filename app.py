53b645a55ca77e5bf648fd2d6e30caff3a808d42
Anders Fischer-Nielsen <andersfischern@me.com>
2016-03-25 10:46:15 +0100

    Implemented user auth, login, token login etc.

--- app.py
+++ app.py
@@ -1,28 +1,130 @@
-from flask import Flask, render_template, request, abort, redirect, url_for, send_from_directory
+#!/usr/bin/env python
+import os
+from flask import Flask, abort, request, jsonify, g, url_for, render_template, redirect
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.httpauth import HTTPBasicAuth
 from werkzeug.utils import secure_filename
-from os import getcwd
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
 
+# initialization
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+# extensions
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None    # valid token, but expired
+        except BadSignature:
+            return None    # invalid token
+        user = User.query.get(data['id'])
+        return user
+
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+
+@app.route('/create_user', methods=['GET'])
+def create_user():
+    return render_template('create_user.html')
+
+
+@app.route('/new_user', methods=['POST'])
+def post_user():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username is None or password is None:
+        abort(400)    # missing arguments
+    if User.query.filter_by(username=username).first() is not None:
+        abort(409)    # existing user
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return (jsonify({'username': user.username}), 201,
+            {'Location': url_for('get_user', id=user.id, _external=True)})
+
+
+@app.route('/users/<int:id>')
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
+
+
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(600)
+    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+
+
+@app.route('/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({'data': 'Hello, %s!' % g.user.username})
 
 
 @app.route('/')
+@auth.login_required
 def hello():
-    return render_template('index.html')
+    return render_template('upload.html')
 
 
 @app.route('/upload_image', methods=['POST'])
 def login():
     pic = request.files["image_upload"]
     filename = secure_filename(pic.filename)
-    pic.save("/Users/Anders/Desktop/" + filename)
-    return redirect(url_for('uploaded_file', filename=filename))
+    pic.save("/www-data/images/" + filename)
+    return redirect(url_for('image', filename=filename))
 
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory("/Users/Anders/Desktop/", filename)
+@app.route('/images/<filename>')
+@auth.login_required
+def image(filename):
+    return send_from_directory("/www-data/images/", filename)
 
 
 if __name__ == '__main__':
+    if not os.path.exists('db.sqlite'):
+        db.create_all()
     app.debug = True
     app.run()
